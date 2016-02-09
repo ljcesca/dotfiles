88948d502d39c0588c48ec34503bde450070ecb1
Lucian Cesca <ljcesca@gmail.com>
2016-02-09 10:04:50 -0800

    add chruby support, add ruby version to prompt

--- zsh/configs/prompt.zsh
+++ zsh/configs/prompt.zsh
@@ -6,9 +6,21 @@ git_prompt_info() {
   fi
 }
 
+ruby_prompt_info() {
+  if command -v chruby > /dev/null 2>&1; then
+    current_ruby=$(chruby | sed -n -e 's/ \* //p' | sed -n -e 's/ruby-//p')
+    if [[ -z $current_ruby ]]; then
+      return
+    fi
+
+
+
+    echo " %{$fg_bold[magenta]%}$current_ruby%{$reset_color%}"
+  fi
+}
+
 setopt promptsubst
 
-# Allow exported PS1 variable to override default prompt.
 if ! env | grep -q '^PS1='; then
-  PS1='${SSH_CONNECTION+"%{$fg_bold[green]%}%n@%m:"}%{$fg_bold[blue]%}%c%{$reset_color%}$(git_prompt_info) %# '
+	PS1='${SSH_CONNECTION+"%{$fg_bold[green]%}%n@%m:"}%{$fg_bold[blue]%}%c%{$reset_color%}$(git_prompt_info)$(ruby_prompt_info) %# '
 fi
