4afc21c5f6e3b5347844fe9f9d2ef60ee794097a
Lucian Cesca <ljcesca@gmail.com>
2018-11-14 10:55:50 -0500

    update chruby setup

--- zsh/configs/post/chruby.zsh
+++ zsh/configs/post/chruby.zsh
@@ -1,4 +1,4 @@
-if [ -s "$(which chruby)" ]; then
+if [ -f /usr/local/opt/chruby/share/chruby/chruby.sh ]; then
   source /usr/local/opt/chruby/share/chruby/chruby.sh
   source /usr/local/opt/chruby/share/chruby/auto.sh
 fi
