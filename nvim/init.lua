822ee57e2367b998a100ef2341f6121ececb2d2b
Lucian Cesca <ljcesca@gmail.com>
2026-01-16 14:32:52 -0500

    start cleaning up neovim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,2 @@
-local load = function(mod)
-  package.loaded[mod] = nil
-  require(mod)
-end
-
-load('user.settings')
-load('user.commands')
-load('user.keymaps')
-require('user.plugins')
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
